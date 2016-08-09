247bbf9c606876d774c9f82e68c1117ab3000739
Adam Miller <maxamillion@fedoraproject.org>
2016-08-09 11:43:11 -0500

    switch to https
    Signed-off-by: Adam Miller <maxamillion@fedoraproject.org>

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ BLOG_AUTHOR = "Adam John Miller"  # (translatable)
 BLOG_TITLE = "maxamillion.sh"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "http://maxamillion.sh/"
+SITE_URL = "https://maxamillion.sh/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://maxamillion.sh/"
